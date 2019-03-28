f69aa87c8c61aba315e429f097400f907f0c4645
UUUUMA <uma.ryo.prog.acc@gmail.com>
2019-03-28 09:50:38 +0900

    [create] if statement ast (BlockStatment is not yet)

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -145,6 +145,33 @@ func (b *Boolean) String() string {
 	return b.Token.Literal
 }
 
+type IfExpression struct {
+	Token       token.Token
+	Condition   Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
+}
+
+func (ie *IfExpression) expressionNode() {}
+func (ie *IfExpression) TokenLiteral() {
+	return ie.Token.Literal
+}
+func (ie *IfExpression) String() {
+	var out bytes.Buffer
+
+	out.WriteString("if")
+	out.WriteString(ie.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(id.Consequence.String())
+
+	if ie.Alternative != nil {
+		out.WriteString("else ")
+		out.WriteString(id.Alternative.String())
+	}
+
+	return out.String()
+}
+
 type PrefixExpression struct {
 	Token    token.Token
 	Operator string
