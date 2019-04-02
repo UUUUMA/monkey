fb2e9444413903252cde749e308ebad132db7a47
UUUUMA <uma.ryo.prog.acc@gmail.com>
2019-04-02 19:38:28 +0900

    [create] implement operator '>', '<', '==', '!='

--- src/evaluator/evaluator.go
+++ src/evaluator/evaluator.go
@@ -58,6 +58,12 @@ func evalInfixExpression(operator string, left object.Object, right object.Objec
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		// compare value as pointer (because Boolean is singleton object)
+		return nativeBoolToBooleanObject(left == right)
+	case operator == "!=":
+		// compare value as pointer (because Boolean is singleton object)
+		return nativeBoolToBooleanObject(left != right)
 	default:
 		return NULL
 	}
@@ -97,6 +103,14 @@ func evalIntegerInfixExpression(operator string, left object.Object, right objec
 		return &object.Integer{Value: leftVal * rightVal}
 	case "/":
 		return &object.Integer{Value: leftVal / rightVal}
+	case "<":
+		return nativeBoolToBooleanObject(leftVal < rightVal)
+	case ">":
+		return nativeBoolToBooleanObject(leftVal > rightVal)
+	case "==":
+		return nativeBoolToBooleanObject(leftVal == rightVal)
+	case "!=":
+		return nativeBoolToBooleanObject(leftVal != rightVal)
 	default:
 		return NULL
 	}
